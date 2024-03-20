92dae2ede86e938be094be645888a7ff1216b2f9
Yesid <yesidrodriguez305@gmail.com>
2024-03-20 10:58:20 -0500

    Fix Hambuerger button actions

--- src/modules/Header.js
+++ src/modules/Header.js
@@ -1,28 +1,28 @@
-import '../css/header.css'
+import '../css/header.css';
 
 export default function Header() {
-  const header = document.createElement('header')
-  const headerDiv = document.createElement('div')
-  const logo = Logo()
-  const searchBar = SearchBar()
-  const navBar = NavBar()
+  const header = document.createElement('header');
+  const headerDiv = document.createElement('div');
+  const logo = Logo();
+  const searchBar = SearchBar();
+  const navBar = NavBar();
 
-  headerDiv.appendChild(logo)
-  headerDiv.innerHTML += searchBar
-  headerDiv.appendChild(navBar)
+  headerDiv.appendChild(logo);
+  headerDiv.innerHTML += searchBar;
+  headerDiv.appendChild(navBar);
 
-  header.appendChild(headerDiv)
-  document.body.insertBefore(header, document.body.firstChild)
+  header.appendChild(headerDiv);
+  document.body.insertBefore(header, document.body.firstChild);
 }
 
 function Logo() {
-  const logo = document.createElement('a')
-  logo.href = 'home'
-  logo.target = '_blank'
-  logo.classList.add('logo')
-  logo.innerHTML = '<img src="/vite.svg" class="logo" alt="Vite logo" />'
+  const logo = document.createElement('a');
+  logo.href = 'home';
+  logo.target = '_blank';
+  logo.classList.add('logo');
+  logo.innerHTML = '<img src="../../public/svg/logo.svg" class="logo" alt="Dreamy Event logo" />';
 
-  return logo
+  return logo;
 }
 
 function SearchBar() {
@@ -31,26 +31,43 @@ function SearchBar() {
       <input type="text" name="search" id="search" placeholder="Buscar">
       <button type="submit" id="searchButton"></button>
     </form>
-  `
+  `;
 
-  return form
+  return form;
 }
 
 function NavBar() {
-  const navBar = document.createElement('nav')
-  navBar.classList.add('nav-bar')
-  const hamburger = `
-    <button type="button" class="hamburguer-menu">
-      <span></span>
-      <span id="user-profile-img"></span>
-    </button>
-  `
+  const navBar = document.createElement('nav');
+  navBar.classList.add('nav-bar');
 
-  const navLinks = NavLinks()
-  navBar.innerHTML = hamburger
-  navBar.appendChild(navLinks)
+  const navLinks = NavLinks();
+  const hamburgerBtn = HamburguerBtn(navLinks);
+  navBar.appendChild(hamburgerBtn);
+  navBar.appendChild(navLinks);
 
-  return navBar
+  return navBar;
+}
+
+function HamburguerBtn(navLinks) {
+  const hamburguerBtn = document.createElement('button');
+  hamburguerBtn.type = 'button';
+  hamburguerBtn.classList.add('hamburguer-menu');
+  hamburguerBtn.innerHTML = `
+    <span></span>
+    <span id="user-profile-img"></span>
+  `;
+
+  hamburguerBtn.addEventListener('click', (e) => {
+    const isClickInsideButton =
+      hamburguerBtn.contains(e.target) || e.target === hamburguerBtn;
+
+    if (isClickInsideButton) {
+      navLinks.classList.toggle('active');
+      navLinks.classList.toggle('inactive');
+    }
+  });
+
+  return hamburguerBtn;
 }
 
 function NavLinks() {
@@ -71,20 +88,21 @@ function NavLinks() {
     { href: 'log-in', text: 'Iniciar sesión', typeUser: 'anonymous' },
     { href: 'sign-up', text: 'Registrarse', typeUser: 'anonymous' },
     { href: 'log-out', text: 'Cerrar sesión', typeUser: 'user-veryfied' },
-  ]
+  ];
 
-  const navLinks = document.createElement('ul')
-  navLinks.classList.add('nav-bar')
+  const navLinks = document.createElement('ul');
+  navLinks.classList.add('nav-bar');
+  navLinks.classList.add('inactive');
 
   links.forEach((link) => {
-    const li = document.createElement('li')
-    const a = document.createElement('a')
-    a.href = link.href
-    a.textContent = link.text
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.href = link.href;
+    a.textContent = link.text;
 
-    li.appendChild(a)
-    navLinks.appendChild(li)
-  })
+    li.appendChild(a);
+    navLinks.appendChild(li);
+  });
 
-  return navLinks
+  return navLinks;
 }
