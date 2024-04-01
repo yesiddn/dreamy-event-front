db223bc9704e0170a27747fa4f7e01ab53d88bc6
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-04-01 17:54:09 -0500

    Update home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import Header from '../../src/templates/Header.js';
 import Hero from '../../src/templates/Hero.js';
+import RecommendedServices from '../templates/RecommendedServices.js';
 
-const Home = () => {
+const Home = (API) => {
   Header();
   Hero();
+  RecommendedServices(API);
 };
 
 export default Home;
