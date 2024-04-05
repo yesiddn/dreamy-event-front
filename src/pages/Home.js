4ff5137c44c3fae57febc1b837880799c12b0097
Camilo <cbenavides851@gmail.com>
2024-04-04 21:24:39 -0500

    The editing service form was created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
 import Header from '../../src/templates/Header.js';
 import Hero from '../../src/templates/Hero.js';
 import RecommendedServices from '../templates/RecommendedServices.js';
-
+import EditServices from '../templates/EditServices.js';
 const Home = (API) => {
   Header();
   Hero();
   RecommendedServices(API);
+  EditServices(API);
 };
 
 export default Home;
