cfd454aea5fb023de246d485e0c456c49c535d15
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-04-08 12:01:51 -0500

    Update alert function

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,9 @@
 import Home from "../pages/Home";
 import CreateService from "../pages/CreateService.js";
-import hola from "../templates/alert.js";
 
 const routes = [
   { path: '/', component: Home },
   { path: '/create-service', component:  CreateService},
-  {path: '/alerts', component: hola},
 ];
 
 export default function router(API) {
