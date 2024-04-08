b2976260c4d70d1d438c3ccd7b64a856f6fea0bf
JuanLeon <leonmolanojuanarley@gmail.com>
2024-04-08 13:54:17 -0500

    Add user registration form that consumes API

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,11 @@
 import Home from '../pages/Home.js';
-import CreateServiceSection from '../pages/CreateService.js';
+import CreateService from '../pages/CreateService.js';
+import CreateUser from '../pages/CreateUser.js'
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/create-service', component: CreateServiceSection },
+  { path: '/create-service', component: CreateService},
+  { path: '/create-user', component: CreateUser}
 ];
 
 export default function router(API) {
