c09fddc47223c1740e998c853c4ac403967ed4dc
JuanLeon <leonmolanojuanarley@gmail.com>
2024-04-14 13:28:16 -0500

    Add template to show created events

---
+++ src/templates/EventSection.js
@@ -0,0 +1,40 @@
+import showEvents from './Events.js';
+import '../styles/my-events.css';
+import getEvents from '../utils/get-event.js';
+
+export default async function EventSection(API){
+    const listEventContainer = document.createElement('section');
+    listEventContainer.classList.add('my-events');
+
+    const eventContainer = document.createElement('div');
+    eventContainer.classList.add('my-events__header');
+    listEventContainer.appendChild(eventContainer);
+     
+    const title = document.createElement('h2');
+    title.innerHTML = "Mis eventos"
+    eventContainer.appendChild(title);
+
+    const newEvent = document.createElement('a');
+    newEvent.href = '/new-event';
+    newEvent.classList.add('my-events__header__create');
+    newEvent.innerHTML = 'Nuevo evento<span></span>';
+    eventContainer.appendChild(newEvent);
+
+    const showMyEvents = document.createElement('div');
+    showMyEvents.classList.add('events-list');
+    listEventContainer.appendChild(showMyEvents);
+
+
+   const events = await getEvents(API);
+
+   if (!events) {
+    return;
+   }
+
+   events.forEach(eventDetail => {
+    const eventCard = showEvents(eventDetail);
+    showMyEvents.appendChild(eventCard);
+});
+
+document.body.appendChild(listEventContainer);
+}
