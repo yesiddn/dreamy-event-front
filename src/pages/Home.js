0433fb87409e272a9b8eafc7635e18eb414542b5
Camilo Andres Benavides Salamanca <cbenavides851@gmail.com>
2024-04-18 00:20:19 -0500

    the filter-bar was created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import Hero from '../templates/Hero.js';
 import Header from '../templates/Header.js';
 import RecommendedServices from '../templates/RecommendedServices.js';
+import FilterBar from '../templates/filter-bar.js';
 
 const Home = (API) => {
   Header();
   Hero();
+  FilterBar();
   RecommendedServices(API);
 };
 
