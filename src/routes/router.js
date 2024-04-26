fbd8f36a7c9a5e9dc477c77c43b24606b1e58327
Yesid <yesidrodriguez305@gmail.com>
2024-04-26 07:31:05 -0500

    chore: Update router.js to use SignUpSupplier component for supplier sign up page

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,12 +3,12 @@ import MyServices from '../pages/MyServices.js';
 import CreateServiceSection from '../pages/CreateService.js';
 import editServiceForm from '../templates/EditServices.js';
 import Login from '../pages/Login.js';
-import SignUpSupplierForm from '../templates/sign-up-supplier-form.js';
 import Signup from '../pages/Signup.js';
 import MyEvents from '../pages/MyEvents.js';
 import CreateEvent from '../pages/CreateEvent.js'; 
 import Alert from '../templates/Alert.js';
 import Error404 from '../templates/error-404.js';
+import SignUpSupplier from '../pages/SignUpSupplier.js';
 
 const routes = [
   { path: '/', component: Home, typeUser: 'public'},
@@ -17,7 +17,7 @@ const routes = [
   { path: '/create-service', component: CreateServiceSection, typeUser: 'supplier'},
   { path: '/edit-service', component: editServiceForm, typeUser: 'supplier'},
   { path: '/sign-up', component: Signup, typeUser: 'public'},
-  { path: '/sign-up-supplier', component: SignUpSupplierForm, typeUser: 'customer'},
+  { path: '/sign-up-supplier', component: SignUpSupplier, typeUser: 'customer'},
   { path: '/my-events', component:MyEvents, typeUser: 'customer'},
   { path: '/new-event', component: CreateEvent, typeUser: 'customer'},
 ];
