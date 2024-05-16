e4cefba3ab67f2c2f432620746d92d99677101c5
Yesid <yesidrodriguez305@gmail.com>
2024-05-16 11:25:36 -0500

    feat: Add image preview to form creation utility function

--- src/utils/create-form.js
+++ src/utils/create-form.js
@@ -1,10 +1,23 @@
 import cancelBtn from './create-cancel-btn';
+import createImagePreview from './create-image-preview';
 import createInput from './create-input';
 
-export default function createForm({ API, inputs, buttons, callback }) {
+export default function createForm({ API, inputs, buttons, serviceDetails, callback }) {
   const form = document.createElement('form');
   form.classList.add('form');
   form.id = 'form';
+
+  if (serviceDetails) {
+    const imagesContainer = document.createElement('div');
+    imagesContainer.classList.add('form__images');
+
+    serviceDetails.images.forEach((image) => {
+      const imagePreview = createImagePreview(image);
+      imagesContainer.appendChild(imagePreview);
+    });
+
+    form.appendChild(imagesContainer);
+  }
 
   const saveBtn = document.createElement('button');
   saveBtn.type = 'button';
