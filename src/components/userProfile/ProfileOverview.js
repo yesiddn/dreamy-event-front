64681efa4a318fa67112d36bc785af1cd512be6f
AlejoShox <pedroalcaro@live.com>
2024-05-15 23:33:13 -0500

    an input of file added by the profile pic to change the profile photo

--- src/components/userProfile/ProfileOverview.js
+++ src/components/userProfile/ProfileOverview.js
@@ -1,26 +1,47 @@
 function ProfileOverview(API, USER) {
-    const overviewContainer = document.createElement('div');
-    overviewContainer.classList.add('user-profile-overview');
+  const overviewContainer = document.createElement('div');
+  overviewContainer.classList.add('user-profile-overview');
 
-    const userPicLabel = document.createElement('label');
-    userPicLabel.classList.add('user-profile-overview-pic')
+  const userPicLabel = document.createElement('label');
+  userPicLabel.classList.add('user-profile-overview-pic')
 
-    const userImg = document.createElement('img');
-    userImg.src = API+'/'+USER.customer.image;
+  const userImg = document.createElement('img');
+  userImg.src = API + '/' + USER.customer.image;
 
-    const userFirstName = document.createElement('h1');
-    userFirstName.textContent = USER.customer.name;
-    userFirstName.classList.add('user-first-name');
+  const userPicUpdate = document.createElement('div');
+  userPicUpdate.classList.add('profile-icon-box');
+  userPicLabel.appendChild(userPicUpdate);
 
-    const userEmailAddress = document.createElement('h1');
-    userEmailAddress.textContent = USER.email;
-    userEmailAddress.classList.add('user-email');
+  const updateSvg = document.createElement('img');
+  updateSvg.src = '../public/svg/update.svg';
+  userPicUpdate.appendChild(updateSvg);
 
-    userPicLabel.appendChild(userImg);
-    overviewContainer.appendChild(userPicLabel);
-    overviewContainer.appendChild(userFirstName);
-    overviewContainer.appendChild(userEmailAddress);
-    return overviewContainer;
-  }
-  
-  export default ProfileOverview;
+  const inputFile = document.createElement('input');
+  inputFile.type = 'file';
+  inputFile.style.display = 'none';
+
+  userPicUpdate.addEventListener('click', function () {
+    inputFile.click();
+  });
+
+  inputFile.addEventListener('change', function () {
+    const file = inputFile.files[0]; // Getting the file
+    console.log(file);
+  });
+
+  const userFirstName = document.createElement('h1');
+  userFirstName.textContent = USER.customer.name;
+  userFirstName.classList.add('user-first-name');
+
+  const userEmailAddress = document.createElement('h1');
+  userEmailAddress.textContent = USER.email;
+  userEmailAddress.classList.add('user-email');
+
+  userPicLabel.appendChild(userImg);
+  overviewContainer.appendChild(userPicLabel);
+  overviewContainer.appendChild(userFirstName);
+  overviewContainer.appendChild(userEmailAddress);
+  return overviewContainer;
+}
+
+export default ProfileOverview;
