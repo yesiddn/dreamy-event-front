808f571449d90c1907e442cf12b522a0d6d56e54
Yesid <yesidrodriguez305@gmail.com>
2024-05-17 10:52:43 -0500

    feat: Update form image preview URL handling

--- src/utils/create-image-preview.js
+++ src/utils/create-image-preview.js
@@ -3,7 +3,9 @@ export default function createImagePreview(image) {
   imageContainer.classList.add('form__images__container');
 
   const img = document.createElement('img');
-  img.src = `http://localhost:3000/api/v1/${image.url}`;
+  img.src = image.local
+    ? image.url
+    : `http://localhost:3000/api/v1/${image.url}`;
   img.classList.add('form__images__preview');
 
   const deleteBtn = document.createElement('button');
