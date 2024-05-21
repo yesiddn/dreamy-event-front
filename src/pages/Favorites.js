266e3b9e3e16186272ce1ab17bd170fc856acae2
AlejoShox <pedroalcaro@live.com>
2024-05-21 11:40:02 -0500

    some essential files has been created to this feature

---
+++ src/pages/Favorites.js
@@ -0,0 +1,8 @@
+import Header from '../templates/Header.js';
+import '../styles/user-profile.css';
+
+export default function Favorites() { 
+    Header();
+    alert('favorites page loaded!')
+};
+
