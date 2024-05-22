1735108f7e4d4e11049df580503056de798e7402
AlejoShox <pedroalcaro@live.com>
2024-05-22 08:06:56 -0500

    a code lined added in the logOut util to destroy the updatedData key in the localstore after logging out

--- src/utils/log-out.js
+++ src/utils/log-out.js
@@ -1,6 +1,7 @@
 import Alert from "../templates/Alert";
 
 export default function logOut() {
+  localStorage.removeItem('updatedData');
   localStorage.removeItem('user');
   Alert('log-out', '/');
 }
