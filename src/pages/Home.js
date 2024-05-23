d9a871361977b4b97142d316ef0e356dc54acf23
AlejoShox <pedroalcaro@live.com>
2024-05-22 22:08:48 -0500

    util completed, so data from the favorites DB table is being loaded in the frontend code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,22 @@ import Header from '../templates/Header.js';
 import RecommendedServices from '../templates/RecommendedServices.js';
 import FilterBar from '../templates/filter-bar.js';
 import Footer from '../templates/Footer.js';
-import getFavorites from '../utils/get-favorites.js'
+import favoritesList from '../utils/get-favorites.js';
 
 const Home = (API, USER) => {
 
   /* crear una condicion para verificar si existe una llave de favoritos en el localstorage */
-  getFavorites(API);
+  if (favoritesList(API)) {
+    favoritesList(API)
+      .then(result => {
+        console.log(result[0]);
+      })
+      .catch(error => {
+        console.error('Error al obtener los favoritos:', error);
+      });
+  } else {
+    'user without favorite data'
+  }
   Header(USER);
   Hero();
   RecommendedServices(API);
--- src/utils/get-favorites.js
+++ src/utils/get-favorites.js
@@ -1,13 +1,20 @@
 /* crear aqui la peticion a la API para traer la lista de los favoritos en la pagina */
+
 import fetchData from "./fetch-data";
 
 const ENDPOINT = "/favorites";
 
-export default async function getFavorites(API, USER) {
-    const response = await fetchData({
-        API: API + ENDPOINT,
-        method: "GET",
-    });
-};
 
-return response.json();
+export default async function getFavorites(API) {
+  const response = await fetchData({
+    API: API + ENDPOINT,
+    method: 'GET'
+  });
+
+  if (response.status === 200) {
+    return response.json();
+  } else {
+    Alert('something-went-wrong');
+    return null;
+  }
+}
