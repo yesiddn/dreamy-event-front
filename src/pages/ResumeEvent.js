1c2dfc1cbe12ade3bab6c0d6a862ea063b736136
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-06-10 16:26:39 -0500

    Refactor ResumeEvent component and related styles

--- src/pages/ResumeEvent.js
+++
@@ -1,7 +0,0 @@
-import Header from '../templates/Header.js';
-import ResumeEvent from '../templates/ResumeEvent.js';
-const ResumeEvents = (API,User) => {
-    Header(User);
-    ResumeEvent(API,User);
-}
-export default ResumeEvents;
